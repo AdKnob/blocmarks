8fb3a56e85c4c1e3089884afbbafd647053a040d
Adrian Knoblauch <adrianknoblauch@me.com>
2017-04-11 19:47:52 +0200

    implemented crud actions for topics controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,10 @@
 class TopicsController < ApplicationController
   def index
+    @topic = Topic.all
   end
 
   def show
+    @topic = Topic.find(params[:id])
   end
 
   def new
@@ -10,5 +12,13 @@ class TopicsController < ApplicationController
   end
 
   def edit
+    @topic.title = params[:topic][:title]
+    if @topic.save
+      flash[:notice] = "Topic was successfully created!"
+      redirect_to @topic
+    else
+      flash.now[:alert] = "There was an error creating the Topic"
+      render :new
+    end
   end
 end
