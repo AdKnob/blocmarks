c9989291faabf3f824ce2c2b58a97f5c59c4c370
Adrian Knoblauch <adrianknoblauch@me.com>
2017-04-12 21:47:43 +0200

    performed crud actions for bookmarks

--- Gemfile
+++ Gemfile
@@ -15,7 +15,8 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'devise'
-gem 'bootstrap', '~> 4.0.0.alpha3'
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'topics/index'
-
-  get 'topics/show'
-
-  get 'topics/new'
-
-  get 'topics/edit'
-
   root :to => "home#index"
-  
+
+  resources :topics do
+    resources :bookmarks
+  end
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
